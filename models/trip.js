bb65767b835cda698cb6258a312f5d0279873bd0
SIH Group <harshnarayanpathak928@gmail.com>
2026-09-03 23:14:56 +0530

    meassage

--- models/trip.js
+++ models/trip.js
@@ -1,18 +1,12 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema();
-
-const tripSchema = new Schema({
-    place : {
-        type : String,
-        required:true
-    },
-    date : {
-        type : Date,
-    },
-    fullItinerary : {
-        type : JSON
-    }
-})
-
-const Trip = mongoose.model("Trip", tripSchema);
-module.exports = Trip;
+const mongoose = require('mongoose');
+const tripSchema = new mongoose.Schema({
+    userName: { type: String, required: true }, // Jis user ne save kiya hai
+    place: String,
+    days: Number,
+    month: String,
+    budgetType: String,
+    // Pura finalAns (JSON object) hum isme save karenge
+    itineraryData: { type: mongoose.Schema.Types.Mixed },
+    savedAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Trip', tripSchema);
